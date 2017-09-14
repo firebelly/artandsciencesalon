3787b704a6b66975a03e43857917bcdff2e81df9
Bryant Smith <bryant.robert.smith@gmail.com>
2017-09-14 17:08:00 -0500

    Finish tablet/desktop nav spacing;  Fix background color for mobile

--- web/app/themes/artandscience/base.php
+++ web/app/themes/artandscience/base.php
@@ -14,7 +14,7 @@ $header_text = get_post_meta($post->ID, '_cmb2_header_text', true);
 <!--[if gt IE 9]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
   <?php get_template_part('templates/head'); ?>
   <body <?php body_class(); ?>>
-    <div class="background"></div>
+    <div class="site-background"></div>
     <!--[if lt IE 9]>
       <div class="alert alert-warning">
         <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
