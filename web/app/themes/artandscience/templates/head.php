0fb9d30c8ec4f041c7645861694ffb92082260ac
Bryant Smith <bryant.robert.smith@gmail.com>
2017-10-10 12:21:16 -0500

    Add favicon

--- web/app/themes/artandscience/templates/head.php
+++ web/app/themes/artandscience/templates/head.php
@@ -4,5 +4,6 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
-  
+  <link rel="icon" type="image/png" href="<?= Roots\Sage\Assets\asset_path('images/favicon.png'); ?>">
+
 </head>
