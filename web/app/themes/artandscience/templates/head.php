ac1570c7b561ee750b136be43ad330d5dca964c8
Bryant Smith <bryant.robert.smith@gmail.com>
2017-10-17 16:44:08 -0500

    Add GA

--- web/app/themes/artandscience/templates/head.php
+++ web/app/themes/artandscience/templates/head.php
@@ -6,4 +6,13 @@
   <?php wp_head(); ?>
   <link rel="icon" type="image/png" href="<?= Roots\Sage\Assets\asset_path('images/favicon.png'); ?>">
 
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-61894369-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-61894369-1');
+  </script>
+
 </head>
