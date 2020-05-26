71f40fa6ee35703c7696c2648fb10d6b52c25903
Nate Beaty <nate@clixel.com>
2020-05-26 16:56:10 -0400

    php70 -> php74

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ set :domain, 'artandsciencesalon.com'
 set :theme, 'artandscience'
 set :login, 'firebelly'
 set :repo_url, 'git@github.com:firebelly/artandsciencesalon.git'
-set :php, 'php70'
+set :php, 'php74'
 
 # Hardcodes branch to always be master
 # This could be overridden in a stage config file
