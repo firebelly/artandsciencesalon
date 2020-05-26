a30557d129fa0ca479215a6c0a4e1083729bba6b
Nate Beaty <nate@clixel.com>
2020-05-26 18:19:59 -0400

    style homepage notice links and add some padding, add "details link" to WP style dropdown

--- web/app/themes/artandscience/lib/fb_init.php
+++ web/app/themes/artandscience/lib/fb_init.php
@@ -38,7 +38,7 @@ function simplify_tinymce($settings) {
     $settings['formats'] = substr($settings['formats'],0,-1).",underline: { inline: 'u', exact: true} }";
   else
     $settings['formats'] = "{ underline: { inline: 'u', exact: true} }";
-  
+
   // What goes into the toolbars. Add 'wp_adv' to get the Toolbar toggle button back
   $settings['toolbar1'] = 'styleselect,bold,italic,underline,strikethrough,formatselect,bullist,numlist,blockquote,link,unlink,hr,wp_more,outdent,indent,AccordionShortcode,AccordionItemShortcode,fullscreen';
   $settings['toolbar2'] = '';
@@ -51,37 +51,46 @@ function simplify_tinymce($settings) {
   // Clear most formatting when pasting text directly in the editor
   $settings['paste_as_text'] = 'true';
 
-  $style_formats = array( 
-    // array( 
-    //   'title' => 'Two Column',
-    //   'block' => 'div',
-    //   'classes' => 'two-column',
-    //   'wrapper' => true,
-    // ),  
-    // array( 
-    //   'title' => 'Three Column',
-    //   'block' => 'div',
-    //   'classes' => 'three-column',
-    //   'wrapper' => true,
-    // ),
-    array( 
-      'title' => 'Button',
+  $style_formats = array(
+    array(
+      'title' => 'Details Link',
       'block' => 'span',
-      'classes' => 'button',
+      'classes' => 'details-link',
     ),
     array(
       'title' => 'Two Image Layout',
       'block' => 'div',
       'classes' => 'image-layout -two-image',
     ),
-    // array( 
-    //   'title' => '» Arrow Link',
-    //   'block' => 'span',
-    //   'classes' => 'arrow-link',
-    // ),
- );  
+ );
   $settings['style_formats'] = json_encode($style_formats);
 
   return $settings;
 }
 add_filter('tiny_mce_before_init', __NAMESPACE__ . '\simplify_tinymce');
+
+/**
+ * Clean up content before saving post
+ */
+function clean_up_content($content) {
+  // Convert <span class="details-link"><a></span> to <a class="details-link"> (can't just add class to element w/ tinymce style formats, has to have wrapper)
+  $content = preg_replace('/<span class=\\\"details-link\\\"><a(.*)<\/a><\/span>/', '<a class=\"details-link\"$1</a>', $content);
+  return $content;
+}
+add_filter('content_save_pre', __NAMESPACE__ . '\\clean_up_content', 10, 1);
+//
+// ... and support for cmb2 wysiwyg fields:
+//
+function cmb2_sanitize_wysiwyg_callback($override_value, $content) {
+  $content = preg_replace('/<span class=\\\"button\\\"><a(.*)<\/a><\/span>/', '<a class=\"button\"$1</a>', $content);
+  return $content;
+}
+add_filter('cmb2_sanitize_wysiwyg', __NAMESPACE__ . '\\cmb2_sanitize_wysiwyg_callback', 10, 2);
+
+/**
+ * Custom Admin styles + JS
+ */
+add_action('admin_enqueue_scripts', function($hook){
+  wp_enqueue_style('fb_wp_admin_css', Assets\asset_path('styles/admin.css'));
+  // wp_enqueue_script('fb_wp_admin_js', Assets\asset_path('scripts/admin.js'), ['jquery'], null, true);
+}, 100);
